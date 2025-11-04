7814150fda53af34cef813ca58c14ba398115df0
Esteban D.Dortta <dortta@yahoo.com>
2025-11-04 15:06:52 -0300

    fix:adicionando venv se não existe

--- run.sh
+++ run.sh
@@ -12,6 +12,12 @@ echo "Loading existing virtual environment."
 if [ ! -d .venv ]; then
 	python3 -mvenv .venv
 fi
+
+dpkg -l | grep libmysqlclient-dev
+if [ $? -ne 0 ]; then
+	sudo apt-get install libmysqlclient-dev
+fi
+
 source .venv/bin/activate
 pip install -r requirements.txt
 python3 create_databases.py 
